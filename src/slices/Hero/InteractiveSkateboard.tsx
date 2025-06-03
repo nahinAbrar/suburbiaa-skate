6748288ca7d31e041d0e60cb8761f767dac17827
Asm Nahin <asmnahin21@gmail.com>
2025-06-03 12:10:55 +0600

    Build Error Fix

--- src/slices/Hero/InteractiveSkateboard.tsx
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Skateboard } from '@/components/Skateboard'
-import { ContactShadows, Environment, Html, OrbitControls } from '@react-three/drei'
+import { ContactShadows, Environment, Html } from '@react-three/drei'
 import { Canvas, ThreeEvent, useThree } from '@react-three/fiber'
 import React, { Suspense, useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
