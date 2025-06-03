9a31a9bee33698e69f154a0a72827943ab0eec8b
Asm Nahin <asmnahin21@gmail.com>
2025-06-03 09:55:38 +0600

    feat: add button links

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -7,27 +7,37 @@ import { CustomizerControlsProvider } from './context'
 import { createClient } from '@/prismicio'
 import Preview from './Preview'
 import { asImageSrc } from '@prismicio/client'
+import Controls from './Controls'
+
+type SearchParams = {
+    wheel?: string
+    deck?: string
+    truck?: string
+    bolt?: string
+}
 
 
+export default async function Page(props: {searchParams: Promise<SearchParams>}) {
 
-export default async function page() {
+    const searchParams = await props.searchParams
 
     const client = createClient()
     const customizerSettings = await client.getSingle('board_customizer')
     const { wheels, decks, metals } = customizerSettings.data
 
-    const defaultWheel = wheels[0]
-    const defaultDeck = decks[0]
-    const defaultTruck = metals[0]
-    const defaultBolt = metals[0]
+    const defaultWheel = wheels.find((wheel) => wheel.uid === searchParams.wheel) ?? wheels[0]
+    const defaultDeck = decks.find((deck) => deck.uid === searchParams.deck) ?? decks[0]
+    const defaultTruck = metals.find((truck) => truck.uid === searchParams.truck) ?? metals[0]
+    const defaultBolt = metals.find((bolt) => bolt.uid === searchParams.bolt) ?? metals[0]
+    
 
     const wheelTextureURLs = wheels
-    .map((wheel) => asImageSrc(wheel.texture))
-    .filter((url): url is string => Boolean(url))
+        .map((wheel) => asImageSrc(wheel.texture))
+        .filter((url): url is string => Boolean(url))
 
     const deckTextureURLs = decks
-    .map((deck) => asImageSrc(deck.texture))
-    .filter((url): url is string => Boolean(url))
+        .map((deck) => asImageSrc(deck.texture))
+        .filter((url): url is string => Boolean(url))
 
     return (
         <div className='flex min-h-screen flex-col lg:flex-row'>
@@ -41,9 +51,9 @@ export default async function page() {
                 <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
 
                     <div className="absolute inset-0">
-                        <Preview 
-                        deckTextureURLs={deckTextureURLs}
-                        wheelTextureURLs={wheelTextureURLs}
+                        <Preview
+                            deckTextureURLs={deckTextureURLs}
+                            wheelTextureURLs={wheelTextureURLs}
                         />
                     </div>
 
@@ -56,6 +66,13 @@ export default async function page() {
 
                 <div className="grow bg-texture bg-zinc-900 text-white ~p-4/6 lg:shrink-0 lg:grow-0">
                     <Heading as="h1" size='sm' className='mb-6 mt-0' >Build Your Board</Heading>
+
+                    <Controls
+                        wheels={wheels}
+                        decks={decks}
+                        metals={metals}
+                        className="mb-6"
+                    />
 
                     <ButtonLink href="/" color='lime' icon='plus'>
                         Add to cart
